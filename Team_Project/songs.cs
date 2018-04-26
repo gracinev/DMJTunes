e6fe6c2afbb208838dd8e9528c7461a7162ffe23
Jessica Rosales <jessrossoccer@hotmail.com>
2018-04-26 09:33:23 -0700

    Commented song list view in albumpage XAML and implementing list view for songs and added song class

---
+++ Team_Project/songs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Project
+{
+    class songs
+    {
+      public string Songname { get; set; }
+
+
+      public songs(string song_name)
+      {
+         this.Songname = song_name;
+      }
+      public static List<songs> list_of_songs()
+      {
+
+         return new List<songs>
+         {
+
+            new songs(
+               song_name: "Hollaback girl"
+            ),
+            
+            new songs(
+               song_name: "All the stars"
+            ),
+
+             new songs(
+               song_name: "Despacito"
+            ),
+
+             new songs(
+               song_name: "Shape of you"
+            ),
+
+            new songs(
+               song_name: "The Weekend"
+            ),
+
+             new songs(
+               song_name: "Bodak Yellow"
+            ),
+
+
+
+
+
+         };
+
+
+
+      }
+
+
+
+      }
+}
