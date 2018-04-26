66c0b6cfc7a783e6b4e042a3dc890f181c18db4e
Jessica Rosales <jessrossoccer@hotmail.com>
2018-04-26 09:52:21 -0700

    changed song class name to Songs.cs

---
+++ Team_Project/Songs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Project
+{
+    class Songs
+    {
+      public string Songname { get; set; }
+
+
+      public Songs(string song_name)
+      {
+         this.Songname = song_name;
+      }
+      public static List<Songs> list_of_songs()
+      {
+
+         return new List<Songs>
+         {
+
+            new Songs(
+               song_name: "Hollaback girl"
+            ),
+            
+            new Songs(
+               song_name: "All the stars"
+            ),
+
+             new Songs(
+               song_name: "Despacito"
+            ),
+
+             new Songs(
+               song_name: "Shape of you"
+            ),
+
+            new Songs(
+               song_name: "The Weekend"
+            ),
+
+             new Songs(
+               song_name: "Bodak Yellow"
+            ),
+
+
+
+
+
+         };
+
+
+
+      }
+
+
+
+      }
+}
